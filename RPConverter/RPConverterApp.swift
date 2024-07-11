b6941d0ebf099f396e3622f1c5af620120b9183a
Pavlo Bondar <ivizey@gmail.com>
2024-07-11 10:21:17 +0300

    Initial Commit

---
+++ RPConverter/RPConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPConverterApp.swift
+//  RPConverter
+//
+//  Created by Pavlo on 11.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RPConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
